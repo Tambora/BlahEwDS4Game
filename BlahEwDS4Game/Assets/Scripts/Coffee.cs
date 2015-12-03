d6360518980abd5797a62bf7b48d69842c2aebb0
alvinleung852 <alvin.ho.leung@gmail.com>
2015-12-03 10:23:15 -0500

    implemented into android
    map and main quad screen with character generation

---
+++ BlahEwDS4Game/Assets/Scripts/Coffee.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coffee : MonoBehaviour {
+
+	public float coffee;
+	public float collectionRate = 10.0f;
+	public float consumeStudy = 10.0f;
+	public float consumeHomework = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+//		GameObject user = GameObject.Find ("");
+
+//		coffee = GetComponent<Coffee>;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if user at coffee location
+		//collectCoffee();
+
+
+	}
+
+	void collectCoffee(){
+		coffee += collectionRate;
+	}
+
+	void consumeCoffee(int consumeType){
+		if(consumeType == 1){
+			coffee -= consumeStudy;
+		}else if(consumeType == 2){
+			coffee -= consumeHomework;
+		}
+	}
+}
